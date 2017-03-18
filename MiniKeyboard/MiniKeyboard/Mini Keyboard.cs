11a72d830737e566238ba161481c6e68a685f3eb
BafrinFattahi <b6021259@my.shu.ac.uk>
2017-03-18 23:59:16 +0000

    Mode Status and ab

--- MiniKeyboard/MiniKeyboard/Mini Keyboard.cs
+++ MiniKeyboard/MiniKeyboard/Mini Keyboard.cs
@@ -32,20 +32,22 @@ namespace MiniKeyboard
         int intMyListIndex = 0;
 
         // Buttons. Identifies which button is being selected be the user. 
-        bool[] boolsButtonPresssed = new bool[19];
+        bool[] BoolIsButtonPressed = new bool[19];
 
         // Prediction. 
         string Str_KeyStrokes;
         // Is the line from the list that has the highest useage 
         int intPredictedIndex;
         int intNumberOfCharacters;
-        int pedictedWorddLength; 
-
+        int pedictedWorddLength;
+        int Number = -1;
+        private string currentFile = "";
         private void Form1_Load(object sender, EventArgs e)
         {
             withinTimer.Interval = intIntervalRequired;
 
-            for (int intWhichButton = 0; intWhichButton <= 18; intWhichButton++) boolsButtonPresssed[intWhichButton] = false;
+            for (int intWhichButton = 0; intWhichButton <= 18; intWhichButton++)
+                BoolIsButtonPressed[intWhichButton] = false;
 
             // Load the Dictionary  loadDictionary();  
         }
@@ -59,5 +61,156 @@ namespace MiniKeyboard
         {
 
         }
+
+        private void btn_Mode_Click(object sender, EventArgs e)
+        {
+          //If the program is on prediction mode switch it to multi-press and vise versa.  
+            if (txt_ModeStatus.Text == "Prediction")
+            {
+                txt_ModeStatus.Text = "Multi-Press";
+            }
+            else if (txt_ModeStatus.Text =="Multi-Press") 
+            {
+                txt_ModeStatus.Text ="Prediction";
+            }
+           
+           
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+          //If first click then set boolean for the button number to true, clear the gloabl list box and apprend variable to 1 (Str_KeySrokes).
+            if (boolFirstVisit == true)
+            {
+                BoolIsButtonPressed(1) = true;
+
+                lstGlobalListbox.Items.Clear();
+                Str_KeyStrokes = "1";
+                //Append Keystrokes to display the variable Str_KeyStrokes
+                txt_KeySequence.AppendText(Str_KeyStrokes);
+                for (int Index = 0; Index <= List_1.Items.Count - 1; Index++)
+                {
+                    lstGlobalListbox.Items.Add(List_1.Items(Index));
+                    //If not first click then enable timer and proceed.
+                    boolFirstVisit = false;
+                    withinTimer.Enabled = true;
+                    Number = Number + 1;
+                    txt_wordBuilder.AppendText(List_1.Items(Number));
+                }
+            }
+            else
+            {
+                //If not first click and is the same click again then.
+                withinTimer.Enabled = false;
+                Number = Number + 1;
+                txt_wordBuilder.Text = txt_wordBuilder.Text.Remove(txt_wordBuilder.TextLength - 1, 1);
+            }
+             if (Number == List_1.Items.Count)
+             {
+                 Number = 0;
+             }
+             if (txt_wordBuilder.AppendText(List_1.Items(Number)))
+             {
+                 withinTimer.Enabled = true;
+             }
+
+        }
+
+        private bool BoolIsButtonPressed(int p)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool BoolsButtonPressed(int p)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool BoolsButtonPresssed(int p)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btn_7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void configureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+          //Code for having the 'Open' dialog box open, i have set to c:\ drive
+	      StreamReader inputStream = default(StreamReader);
+	      OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
+	      OpenFileDialog1.InitialDirectory = "c:\\";
+	   if (OpenFileDialog1.ShowDialog() == DialogResult.OK)
+       {
+		   currentFile = OpenFileDialog1.FileName;
+		   inputStream = File.OpenText(currentFile);
+		   txt_Main.Text = inputStream.ReadToEnd();
+		  inputStream.Close();
+	
+        }
     }
+
+        private void saveASToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            //Code for having the 'SaveAs' dialog box open, i have also set to c:\ drive
+
+
+	        StreamWriter outputStream = default(StreamWriter);
+	        SaveFileDialog SaveFileDialog1 = new SaveFileDialog();
+	        SaveFileDialog1.InitialDirectory = "c:\\";
+	       if (SaveFileDialog1.ShowDialog() == DialogResult.OK)
+           {
+	    	 currentFile = SaveFileDialog1.FileName;
+		     outputStream = File.CreateText(currentFile);
+		     outputStream.Write(txt_Main.Text);
+		     outputStream.Close();
+	}
+}
+
+        private void saveToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            //Code for the 'Save' option, I have set it to save the text in the main window
+            if (!string.IsNullOrEmpty(currentFile))
+            {
+                StreamWriter outputStream = File.CreateText(currentFile);
+                outputStream.Write(txt_Main.Text);
+                outputStream.Close();
+            }
+
+        }
+
+
+        }
 }
